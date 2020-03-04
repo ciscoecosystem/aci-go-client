bf0c588004e21af1a1db24c01478326c80331466
Aparna <aparnathumar1599@gmail.com>
2020-03-04 12:16:43 +0530

    modify infraleafs_servise

--- client/infraLeafS_service.go
+++ client/infraLeafS_service.go
@@ -3,34 +3,20 @@ package client
 import (
 	"fmt"
 
-	"github.com/ciscoecosystem/aci-go-client/models"
 	"github.com/ciscoecosystem/aci-go-client/container"
-
-
-
-	
-
-
+	"github.com/ciscoecosystem/aci-go-client/models"
 )
 
-
-
-
-
-
-
-
-
-func (sm *ServiceManager) CreateSwitchAssociation(switch_association_type string , name string ,leaf_profile string , description string, infraLeafSattr models.SwitchAssociationAttributes) (*models.SwitchAssociation, error) {	
-	rn := fmt.Sprintf("leaves-%s-typ-%s",name, switch_association_type)
-	parentDn := fmt.Sprintf("uni/infra/nprof-%s", leaf_profile )
+func (sm *ServiceManager) CreateSwitchAssociation(switch_association_type string, name string, leaf_profile string, description string, infraLeafSattr models.SwitchAssociationAttributes) (*models.SwitchAssociation, error) {
+	rn := fmt.Sprintf("leaves-%s-typ-%s", name, switch_association_type)
+	parentDn := fmt.Sprintf("uni/infra/nprof-%s", leaf_profile)
 	infraLeafS := models.NewSwitchAssociation(rn, parentDn, description, infraLeafSattr)
 	err := sm.Save(infraLeafS)
 	return infraLeafS, err
 }
 
-func (sm *ServiceManager) ReadSwitchAssociation(switch_association_type string , name string ,leaf_profile string ) (*models.SwitchAssociation, error) {
-	dn := fmt.Sprintf("uni/infra/nprof-%s/leaves-%s-typ-%s", leaf_profile ,name , ,switch_association_type )    
+func (sm *ServiceManager) ReadSwitchAssociation(switch_association_type string, name string, leaf_profile string) (*models.SwitchAssociation, error) {
+	dn := fmt.Sprintf("uni/infra/nprof-%s/leaves-%s-typ-%s", leaf_profile, name, switch_association_type)
 	cont, err := sm.Get(dn)
 	if err != nil {
 		return nil, err
@@ -40,34 +26,34 @@ func (sm *ServiceManager) ReadSwitchAssociation(switch_association_type string ,
 	return infraLeafS, nil
 }
 
-func (sm *ServiceManager) DeleteSwitchAssociation(switch_association_type string , name string ,leaf_profile string ) error {
-	dn := fmt.Sprintf("uni/infra/nprof-%s/leaves-%s-typ-%s", leaf_profile ,name , ,switch_association_type )
+func (sm *ServiceManager) DeleteSwitchAssociation(switch_association_type string, name string, leaf_profile string) error {
+	dn := fmt.Sprintf("uni/infra/nprof-%s/leaves-%s-typ-%s", leaf_profile, name, switch_association_type)
 	return sm.DeleteByDn(dn, models.InfraleafsClassName)
 }
 
-func (sm *ServiceManager) UpdateSwitchAssociation(switch_association_type string , name string ,leaf_profile string  ,description string, infraLeafSattr models.SwitchAssociationAttributes) (*models.SwitchAssociation, error) {
-	rn := fmt.Sprintf("leaves-%s-typ-%s",name, switch_association_type)
-	parentDn := fmt.Sprintf("uni/infra/nprof-%s", leaf_profile )
+func (sm *ServiceManager) UpdateSwitchAssociation(switch_association_type string, name string, leaf_profile string, description string, infraLeafSattr models.SwitchAssociationAttributes) (*models.SwitchAssociation, error) {
+	rn := fmt.Sprintf("leaves-%s-typ-%s", name, switch_association_type)
+	parentDn := fmt.Sprintf("uni/infra/nprof-%s", leaf_profile)
 	infraLeafS := models.NewSwitchAssociation(rn, parentDn, description, infraLeafSattr)
 
-    infraLeafS.Status = "modified"
+	infraLeafS.Status = "modified"
 	err := sm.Save(infraLeafS)
 	return infraLeafS, err
 
 }
 
-func (sm *ServiceManager) ListSwitchAssociation(leaf_profile string ) ([]*models.SwitchAssociation, error) {
+func (sm *ServiceManager) ListSwitchAssociation(leaf_profile string) ([]*models.SwitchAssociation, error) {
 
-	baseurlStr := "/api/node/class"	
-	dnUrl := fmt.Sprintf("%s/uni/infra/nprof-%s/infraLeafS.json", baseurlStr , leaf_profile )
-    
-    cont, err := sm.GetViaURL(dnUrl)
+	baseurlStr := "/api/node/class"
+	dnUrl := fmt.Sprintf("%s/uni/infra/nprof-%s/infraLeafS.json", baseurlStr, leaf_profile)
+
+	cont, err := sm.GetViaURL(dnUrl)
 	list := models.SwitchAssociationListFromContainer(cont)
 
 	return list, err
 }
 
-func (sm *ServiceManager) CreateRelationinfraRsAccNodePGrpFromSwitchAssociation( parentDn, tnInfraAccNodePGrpName string) error {
+func (sm *ServiceManager) CreateRelationinfraRsAccNodePGrpFromSwitchAssociation(parentDn, tnInfraAccNodePGrpName string) error {
 	dn := fmt.Sprintf("%s/rsaccNodePGrp", parentDn)
 	containerJSON := []byte(fmt.Sprintf(`{
 		"%s": {
@@ -76,7 +62,7 @@ func (sm *ServiceManager) CreateRelationinfraRsAccNodePGrpFromSwitchAssociation(
 								
 			}
 		}
-	}`, "infraRsAccNodePGrp", dn,tnInfraAccNodePGrpName))
+	}`, "infraRsAccNodePGrp", dn, tnInfraAccNodePGrpName))
 
 	jsonPayload, err := container.ParseJSON(containerJSON)
 	if err != nil {
@@ -97,29 +83,23 @@ func (sm *ServiceManager) CreateRelationinfraRsAccNodePGrpFromSwitchAssociation(
 	return nil
 }
 
-func (sm *ServiceManager) DeleteRelationinfraRsAccNodePGrpFromSwitchAssociation(parentDn string) error{
+func (sm *ServiceManager) DeleteRelationinfraRsAccNodePGrpFromSwitchAssociation(parentDn string) error {
 	dn := fmt.Sprintf("%s/rsaccNodePGrp", parentDn)
-	return sm.DeleteByDn(dn , "infraRsAccNodePGrp")
+	return sm.DeleteByDn(dn, "infraRsAccNodePGrp")
 }
 
-func (sm *ServiceManager) ReadRelationinfraRsAccNodePGrpFromSwitchAssociation( parentDn string) (interface{},error) {
-	baseurlStr := "/api/node/class"	
-	dnUrl := fmt.Sprintf("%s/%s/%s.json",baseurlStr,parentDn,"infraRsAccNodePGrp")
+func (sm *ServiceManager) ReadRelationinfraRsAccNodePGrpFromSwitchAssociation(parentDn string) (interface{}, error) {
+	baseurlStr := "/api/node/class"
+	dnUrl := fmt.Sprintf("%s/%s/%s.json", baseurlStr, parentDn, "infraRsAccNodePGrp")
 	cont, err := sm.GetViaURL(dnUrl)
 
-	contList := models.ListFromContainer(cont,"infraRsAccNodePGrp")
-	
+	contList := models.ListFromContainer(cont, "infraRsAccNodePGrp")
+
 	if len(contList) > 0 {
 		dat := models.G(contList[0], "tnInfraAccNodePGrpName")
 		return dat, err
 	} else {
-		return nil,err
+		return nil, err
 	}
-		
-
-
-
-
 
 }
-
