6dc6a705b20ed892b2a5d87726665a35b64f591f
Aparna <aparnathumar1599@gmail.com>
2020-03-18 11:47:54 +0530

    modified fabricNodeBlk_service added

--- client/fabricNodeBlk_FirmWare_service.go
+++ client/fabricNodeBlk_FirmWare_service.go
@@ -9,12 +9,12 @@ import (
 func (sm *ServiceManager) CreateNodeBlockFW(name string, firmware_group string, description string, fabricNodeBlkattr models.NodeBlockAttributes) (*models.NodeBlock, error) {
 	rn := fmt.Sprintf("nodeblk-%s", name)
 	parentDn := fmt.Sprintf("uni/fabric/fwgrp-%s", firmware_group)
-	fabricNodeBlk := models.NewNodeBlock(rn, parentDn, description, fabricNodeBlkattr)
+	fabricNodeBlk := models.NewNodeBlockFW(rn, parentDn, description, fabricNodeBlkattr)
 	err := sm.Save(fabricNodeBlk)
 	return fabricNodeBlk, err
 }
 
-func (sm *ServiceManager) ReadNodeBlockFW(name string, firmware_group string) (*models.NodeBlock, error) {
+func (sm *ServiceManager) ReadNodeBlockFW(name string, firmware_group string) (*models.NodeBlockFW, error) {
 	dn := fmt.Sprintf("uni/fabric/fwgrp-%s/nodeblk-%s", firmware_group, name)
 	cont, err := sm.Get(dn)
 	if err != nil {
@@ -27,13 +27,13 @@ func (sm *ServiceManager) ReadNodeBlockFW(name string, firmware_group string) (*
 
 func (sm *ServiceManager) DeleteNodeBlockFW(name string, firmware_group string) error {
 	dn := fmt.Sprintf("uni/fabric/fwgrp-%s/nodeblk-%s", firmware_group, name)
-	return sm.DeleteByDn(dn, models.FabricnodeblkClassName)
+	return sm.DeleteByDn(dn, models.FabricnodeblkClassNameFW)
 }
 
-func (sm *ServiceManager) UpdateNodeBlockFW(name string, firmware_group string, description string, fabricNodeBlkattr models.NodeBlockAttributes) (*models.NodeBlock, error) {
+func (sm *ServiceManager) UpdateNodeBlockFW(name string, firmware_group string, description string, fabricNodeBlkattr models.NodeBlockAttributesFW) (*models.NodeBlockFW, error) {
 	rn := fmt.Sprintf("nodeblk-%s", name)
 	parentDn := fmt.Sprintf("uni/fabric/fwgrp-%s", firmware_group)
-	fabricNodeBlk := models.NewNodeBlock(rn, parentDn, description, fabricNodeBlkattr)
+	fabricNodeBlk := models.NewNodeBlockFW(rn, parentDn, description, fabricNodeBlkattr)
 
 	fabricNodeBlk.Status = "modified"
 	err := sm.Save(fabricNodeBlk)
@@ -41,7 +41,7 @@ func (sm *ServiceManager) UpdateNodeBlockFW(name string, firmware_group string, 
 
 }
 
-func (sm *ServiceManager) ListNodeBlockFW(firmware_group string) ([]*models.NodeBlock, error) {
+func (sm *ServiceManager) ListNodeBlockFW(firmware_group string) ([]*models.NodeBlockFW, error) {
 
 	baseurlStr := "/api/node/class"
 	dnUrl := fmt.Sprintf("%s/uni/fabric/fwgrp-%s/fabricNodeBlk.json", baseurlStr, firmware_group)
--- client/fabricNodeBlk_MaintGrp_service.go
+++ client/fabricNodeBlk_MaintGrp_service.go
@@ -9,12 +9,12 @@ import (
 func (sm *ServiceManager) CreateNodeBlockMG(name string, pod_maintenance_group string, description string, fabricNodeBlkattr models.NodeBlockAttributes) (*models.NodeBlock, error) {
 	rn := fmt.Sprintf("nodeblk-%s", name)
 	parentDn := fmt.Sprintf("uni/fabric/maintgrp-%s", pod_maintenance_group)
-	fabricNodeBlk := models.NewNodeBlock(rn, parentDn, description, fabricNodeBlkattr)
+	fabricNodeBlk := models.NewNodeBlockMG(rn, parentDn, description, fabricNodeBlkattr)
 	err := sm.Save(fabricNodeBlk)
 	return fabricNodeBlk, err
 }
 
-func (sm *ServiceManager) ReadNodeBlockMG(name string, pod_maintenance_group string) (*models.NodeBlock, error) {
+func (sm *ServiceManager) ReadNodeBlockMG(name string, pod_maintenance_group string) (*models.NodeBlockMG, error) {
 	dn := fmt.Sprintf("uni/fabric/maintgrp-%s/nodeblk-%s", pod_maintenance_group, name)
 	cont, err := sm.Get(dn)
 	if err != nil {
@@ -27,13 +27,13 @@ func (sm *ServiceManager) ReadNodeBlockMG(name string, pod_maintenance_group str
 
 func (sm *ServiceManager) DeleteNodeBlockMG(name string, pod_maintenance_group string) error {
 	dn := fmt.Sprintf("uni/fabric/maintgrp-%s/nodeblk-%s", pod_maintenance_group, name)
-	return sm.DeleteByDn(dn, models.FabricnodeblkClassName)
+	return sm.DeleteByDn(dn, models.FabricnodeblkClassNameMG)
 }
 
-func (sm *ServiceManager) UpdateNodeBlockMG(name string, pod_maintenance_group string, description string, fabricNodeBlkattr models.NodeBlockAttributes) (*models.NodeBlock, error) {
+func (sm *ServiceManager) UpdateNodeBlockMG(name string, pod_maintenance_group string, description string, fabricNodeBlkattr models.NodeBlockAttributesMG) (*models.NodeBlockMG, error) {
 	rn := fmt.Sprintf("nodeblk-%s", name)
 	parentDn := fmt.Sprintf("uni/fabric/maintgrp-%s", pod_maintenance_group)
-	fabricNodeBlk := models.NewNodeBlock(rn, parentDn, description, fabricNodeBlkattr)
+	fabricNodeBlk := models.NewNodeBlockMG(rn, parentDn, description, fabricNodeBlkattr)
 
 	fabricNodeBlk.Status = "modified"
 	err := sm.Save(fabricNodeBlk)
@@ -41,7 +41,7 @@ func (sm *ServiceManager) UpdateNodeBlockMG(name string, pod_maintenance_group s
 
 }
 
-func (sm *ServiceManager) ListNodeBlockMG(pod_maintenance_group string) ([]*models.NodeBlock, error) {
+func (sm *ServiceManager) ListNodeBlockMG(pod_maintenance_group string) ([]*models.NodeBlockMG, error) {
 
 	baseurlStr := "/api/node/class"
 	dnUrl := fmt.Sprintf("%s/uni/fabric/maintgrp-%s/fabricNodeBlk.json", baseurlStr, pod_maintenance_group)
