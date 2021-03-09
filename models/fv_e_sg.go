0b01e71dc8232dca8ef61b671169eb6f6e1d19c3
cindy zhao <xinyuezhao96@gmail.com>
2021-03-09 10:48:25 -0800

    Fix as commented

--- models/fv_e_sg.go
+++ models/fv_e_sg.go
@@ -8,49 +8,30 @@ import (
 	"github.com/ciscoecosystem/aci-go-client/container"
 )
 
-const FvesgClassName = "fvESg"
+const (
+	  FvesgClassName = "fvESg"
+	  ParentDn       = "uni/tn-%s/ap-%s"
+	  Dn             = "uni/tn-%s/ap-%s/esg-%s"
+	  Rn			 = "esg-%s"
+)
 
 type EndpointSecurityGroup struct {
 	BaseAttributes
     EndpointSecurityGroupAttributes 
 }
-  
-type EndpointSecurityGroupAttributes struct {
-	
-	
+
+type EndpointSecurityGroupAttributes struct {	
 	Name string `json:",omitempty"`
-	
-	
-    
 	Annotation       string `json:",omitempty"`
-	
-    
 	ExceptionTag       string `json:",omitempty"`
-	
-    
 	FloodOnEncap       string `json:",omitempty"`
-	
-    
 	MatchT       string `json:",omitempty"`
-	
-    
 	NameAlias       string `json:",omitempty"`
-	
-    
 	PcEnfPref       string `json:",omitempty"`
-	
-    
 	PrefGrMemb       string `json:",omitempty"`
-	
-    
 	Prio       string `json:",omitempty"`
-	
-    
 	Userdom       string `json:",omitempty"`
-	
-    
 }
-   
 
 func NewEndpointSecurityGroup(fvESgRn, parentDn, description string, fvESgattr EndpointSecurityGroupAttributes) *EndpointSecurityGroup {
 	dn := fmt.Sprintf("%s/%s", parentDn, fvESgRn)  
@@ -62,9 +43,7 @@ func NewEndpointSecurityGroup(fvESgRn, parentDn, description string, fvESgattr E
 			ClassName:         FvesgClassName,
 			Rn:                fvESgRn,
 		},
-        
 		EndpointSecurityGroupAttributes: fvESgattr,
-         
 	}
 }
 
@@ -73,47 +52,20 @@ func (fvESg *EndpointSecurityGroup) ToMap() (map[string]string, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	
-	
 	A(fvESgMap, "name",fvESg.Name)
-	
-	
-    
 	A(fvESgMap, "annotation",fvESg.Annotation)
-	
-    
 	A(fvESgMap, "exceptionTag",fvESg.ExceptionTag)
-	
-    
 	A(fvESgMap, "floodOnEncap",fvESg.FloodOnEncap)
-	
-    
 	A(fvESgMap, "matchT",fvESg.MatchT)
-	
-    
 	A(fvESgMap, "nameAlias",fvESg.NameAlias)
-	
-    
 	A(fvESgMap, "pcEnfPref",fvESg.PcEnfPref)
-	
-    
 	A(fvESgMap, "prefGrMemb",fvESg.PrefGrMemb)
-	
-    
 	A(fvESgMap, "prio",fvESg.Prio)
-	
-    
 	A(fvESgMap, "userdom",fvESg.Userdom)
-	
-    
-	
-
 	return fvESgMap, err
 }
 
 func EndpointSecurityGroupFromContainerList(cont *container.Container, index int) *EndpointSecurityGroup {
-
 	EndpointSecurityGroupCont := cont.S("imdata").Index(index).S(FvesgClassName, "attributes")
 	return &EndpointSecurityGroup{
 		BaseAttributes{
@@ -124,59 +76,31 @@ func EndpointSecurityGroupFromContainerList(cont *container.Container, index int
 			Rn:                G(EndpointSecurityGroupCont, "rn"),
 		},
         
-		EndpointSecurityGroupAttributes{
-		
-		
+		EndpointSecurityGroupAttributes{	
 			Name : G(EndpointSecurityGroupCont, "name"),
-		
-		
-        
-	        Annotation : G(EndpointSecurityGroupCont, "annotation"),
-		
-        
-	        ExceptionTag : G(EndpointSecurityGroupCont, "exceptionTag"),
-		
-        
-	        FloodOnEncap : G(EndpointSecurityGroupCont, "floodOnEncap"),
-		
-        
-	        MatchT : G(EndpointSecurityGroupCont, "matchT"),
-		
-        
-	        NameAlias : G(EndpointSecurityGroupCont, "nameAlias"),
-		
-        
-	        PcEnfPref : G(EndpointSecurityGroupCont, "pcEnfPref"),
-		
-        
-	        PrefGrMemb : G(EndpointSecurityGroupCont, "prefGrMemb"),
-		
-        
-	        Prio : G(EndpointSecurityGroupCont, "prio"),
-		
-        
-	        Userdom : G(EndpointSecurityGroupCont, "userdom"),
-		
-        		
+			Annotation : G(EndpointSecurityGroupCont, "annotation"),
+			ExceptionTag : G(EndpointSecurityGroupCont, "exceptionTag"),
+			FloodOnEncap : G(EndpointSecurityGroupCont, "floodOnEncap"),
+			MatchT : G(EndpointSecurityGroupCont, "matchT"),   
+			NameAlias : G(EndpointSecurityGroupCont, "nameAlias"),
+			PcEnfPref : G(EndpointSecurityGroupCont, "pcEnfPref"),
+			PrefGrMemb : G(EndpointSecurityGroupCont, "prefGrMemb"),
+			Prio : G(EndpointSecurityGroupCont, "prio"),
+			Userdom : G(EndpointSecurityGroupCont, "userdom"),
         },
         
 	}
 }
 
 func EndpointSecurityGroupFromContainer(cont *container.Container) *EndpointSecurityGroup {
-
 	return EndpointSecurityGroupFromContainerList(cont, 0)
 }
 
 func EndpointSecurityGroupListFromContainer(cont *container.Container) []*EndpointSecurityGroup {
 	length, _ := strconv.Atoi(G(cont, "totalCount"))
-
 	arr := make([]*EndpointSecurityGroup, length)
-
 	for i := 0; i < length; i++ {
-
 		arr[i] = EndpointSecurityGroupFromContainerList(cont, i)
 	}
-
 	return arr
 }
