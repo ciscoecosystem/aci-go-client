0c8c1d4d3e940679ef5f2a6fa49cd4a4433292e0
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2021-09-15 15:04:12 +0530

    added model and service file for recurring_window resource

--- models/trig_recurr_window_p.go
+++ models/trig_recurr_window_p.go
@@ -6,45 +6,47 @@ import (
 
 	"github.com/ciscoecosystem/aci-go-client/container"
 )
+
 const (
-	DntrigRecurrWindowP                = "uni/fabric/schedp-%s/recurrwinp-%s"
-	RntrigRecurrWindowP                = "recurrwinp-%s"
-	ParentDntrigRecurrWindowP          = "uni/fabric/schedp-%s"
+	DntrigRecurrWindowP        = "uni/fabric/schedp-%s/recurrwinp-%s"
+	RntrigRecurrWindowP        = "recurrwinp-%s"
+	ParentDntrigRecurrWindowP  = "uni/fabric/schedp-%s"
 	TrigrecurrwindowpClassName = "trigRecurrWindowP"
 )
 
 type RecurringWindow struct {
 	BaseAttributes
 	NameAliasAttribute
-    RecurringWindowAttributes 
+	RecurringWindowAttributes
 }
-  
-type RecurringWindowAttributes struct {
-    ConcurCap       string `json:",omitempty"`
-    Day       string `json:",omitempty"`
-    Hour       string `json:",omitempty"`
-    Minute       string `json:",omitempty"`
-    Name       string `json:",omitempty"`
-    NodeUpgInterval       string `json:",omitempty"`
-    ProcBreak       string `json:",omitempty"`
-    ProcCap       string `json:",omitempty"`
-    TimeCap       string `json:",omitempty"`
-    } 
 
-func NewRecurringWindow(trigRecurrWindowPRn, parentDn, description, nameAlias string, trigRecurrWindowPAttr RecurringWindowAttributes) *RecurringWindow {
-	dn := fmt.Sprintf("%s/%s", parentDn, trigRecurrWindowPRn)  
+type RecurringWindowAttributes struct {
+	ConcurCap       string `json:",omitempty"`
+	Day             string `json:",omitempty"`
+	Hour            string `json:",omitempty"`
+	Minute          string `json:",omitempty"`
+	Name            string `json:",omitempty"`
+	NodeUpgInterval string `json:",omitempty"`
+	ProcBreak       string `json:",omitempty"`
+	ProcCap         string `json:",omitempty"`
+	TimeCap         string `json:",omitempty"`
+	Annotation      string `json:",omitempty"`
+}
+
+func NewRecurringWindow(trigRecurrWindowPRn, parentDn, nameAlias string, trigRecurrWindowPAttr RecurringWindowAttributes) *RecurringWindow {
+	dn := fmt.Sprintf("%s/%s", parentDn, trigRecurrWindowPRn)
 	return &RecurringWindow{
 		BaseAttributes: BaseAttributes{
 			DistinguishedName: dn,
-			Description:       description,
-			Status:            "created, modified",
-			ClassName:         TrigrecurrwindowpClassName,
-			Rn:                trigRecurrWindowPRn,
+			//Description:       description,
+			Status:    "created, modified",
+			ClassName: TrigrecurrwindowpClassName,
+			Rn:        trigRecurrWindowPRn,
 		},
 		NameAliasAttribute: NameAliasAttribute{
 			NameAlias: nameAlias,
 		},
-        RecurringWindowAttributes: trigRecurrWindowPAttr, 
+		RecurringWindowAttributes: trigRecurrWindowPAttr,
 	}
 }
 
@@ -60,16 +62,17 @@ func (trigRecurrWindowP *RecurringWindow) ToMap() (map[string]string, error) {
 	for key, value := range alias {
 		A(trigRecurrWindowPMap, key, value)
 	}
-    A(trigRecurrWindowPMap, "concurCap",trigRecurrWindowP.ConcurCap)
-    A(trigRecurrWindowPMap, "day",trigRecurrWindowP.Day)
-    A(trigRecurrWindowPMap, "hour",trigRecurrWindowP.Hour)
-    A(trigRecurrWindowPMap, "minute",trigRecurrWindowP.Minute)
-    A(trigRecurrWindowPMap, "name",trigRecurrWindowP.Name)
-    A(trigRecurrWindowPMap, "nodeUpgInterval",trigRecurrWindowP.NodeUpgInterval)
-    A(trigRecurrWindowPMap, "procBreak",trigRecurrWindowP.ProcBreak)
-    A(trigRecurrWindowPMap, "procCap",trigRecurrWindowP.ProcCap)
-    A(trigRecurrWindowPMap, "timeCap",trigRecurrWindowP.TimeCap)
-    return trigRecurrWindowPMap, err
+	A(trigRecurrWindowPMap, "concurCap", trigRecurrWindowP.ConcurCap)
+	A(trigRecurrWindowPMap, "day", trigRecurrWindowP.Day)
+	A(trigRecurrWindowPMap, "hour", trigRecurrWindowP.Hour)
+	A(trigRecurrWindowPMap, "minute", trigRecurrWindowP.Minute)
+	A(trigRecurrWindowPMap, "name", trigRecurrWindowP.Name)
+	A(trigRecurrWindowPMap, "nodeUpgInterval", trigRecurrWindowP.NodeUpgInterval)
+	A(trigRecurrWindowPMap, "procBreak", trigRecurrWindowP.ProcBreak)
+	A(trigRecurrWindowPMap, "procCap", trigRecurrWindowP.ProcCap)
+	A(trigRecurrWindowPMap, "timeCap", trigRecurrWindowP.TimeCap)
+	A(trigRecurrWindowPMap, "annotation", trigRecurrWindowP.Annotation)
+	return trigRecurrWindowPMap, err
 }
 
 func RecurringWindowFromContainerList(cont *container.Container, index int) *RecurringWindow {
@@ -77,25 +80,26 @@ func RecurringWindowFromContainerList(cont *container.Container, index int) *Rec
 	return &RecurringWindow{
 		BaseAttributes{
 			DistinguishedName: G(RecurringWindowCont, "dn"),
-			Description:       G(RecurringWindowCont, "descr"),
-			Status:            G(RecurringWindowCont, "status"),
-			ClassName:         TrigrecurrwindowpClassName,
-			Rn:                G(RecurringWindowCont, "rn"),
+			//Description:       G(RecurringWindowCont, "descr"),
+			Status:    G(RecurringWindowCont, "status"),
+			ClassName: TrigrecurrwindowpClassName,
+			Rn:        G(RecurringWindowCont, "rn"),
 		},
 		NameAliasAttribute{
 			NameAlias: G(RecurringWindowCont, "nameAlias"),
 		},
-        RecurringWindowAttributes{
-        ConcurCap : G(RecurringWindowCont, "concurCap"),
-        Day : G(RecurringWindowCont, "day"),
-        Hour : G(RecurringWindowCont, "hour"),
-        Minute : G(RecurringWindowCont, "minute"),
-        Name : G(RecurringWindowCont, "name"),
-        NodeUpgInterval : G(RecurringWindowCont, "nodeUpgInterval"),
-        ProcBreak : G(RecurringWindowCont, "procBreak"),
-        ProcCap : G(RecurringWindowCont, "procCap"),
-        TimeCap : G(RecurringWindowCont, "timeCap"),
-        },
+		RecurringWindowAttributes{
+			ConcurCap:       G(RecurringWindowCont, "concurCap"),
+			Day:             G(RecurringWindowCont, "day"),
+			Hour:            G(RecurringWindowCont, "hour"),
+			Minute:          G(RecurringWindowCont, "minute"),
+			Name:            G(RecurringWindowCont, "name"),
+			NodeUpgInterval: G(RecurringWindowCont, "nodeUpgInterval"),
+			ProcBreak:       G(RecurringWindowCont, "procBreak"),
+			ProcCap:         G(RecurringWindowCont, "procCap"),
+			TimeCap:         G(RecurringWindowCont, "timeCap"),
+			Annotation:      G(RecurringWindowCont, "annotation"),
+		},
 	}
 }
 
