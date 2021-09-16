45de71128d8824e6a6ff3c1ffc15e494dc669298
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2021-09-16 13:40:07 +0530

    added client and model for remote path of a file

--- client/fileRemotePath_service.go
+++ client/fileRemotePath_service.go
@@ -1,26 +1,21 @@
 package client
 
-
-
 import (
-	
 	"fmt"
 
-	"github.com/ciscoecosystem/aci-go-client/models"
 	"github.com/ciscoecosystem/aci-go-client/container"
-	
+	"github.com/ciscoecosystem/aci-go-client/models"
 )
 
-
-func (sm *ServiceManager) CreateRemotePathofaFile(name string, description string, nameAlias string, fileRemotePathAttr models.RemotePathofaFileAttributes) (*models.RemotePathofaFile, error) {	
-	rn := fmt.Sprintf(models.RnfileRemotePath , name)
-	parentDn := fmt.Sprintf(models.ParentDnfileRemotePath, )
+func (sm *ServiceManager) CreateRemotePathofaFile(name string, description string, nameAlias string, fileRemotePathAttr models.RemotePathofaFileAttributes) (*models.RemotePathofaFile, error) {
+	rn := fmt.Sprintf(models.RnfileRemotePath, name)
+	parentDn := fmt.Sprintf(models.ParentDnfileRemotePath)
 	fileRemotePath := models.NewRemotePathofaFile(rn, parentDn, description, nameAlias, fileRemotePathAttr)
 	err := sm.Save(fileRemotePath)
 	return fileRemotePath, err
 }
 
-func (sm *ServiceManager) ReadRemotePathofaFile(name string, ) (*models.RemotePathofaFile, error) {
+func (sm *ServiceManager) ReadRemotePathofaFile(name string) (*models.RemotePathofaFile, error) {
 	dn := fmt.Sprintf(models.DnfileRemotePath, name)
 	cont, err := sm.Get(dn)
 	if err != nil {
@@ -30,23 +25,23 @@ func (sm *ServiceManager) ReadRemotePathofaFile(name string, ) (*models.RemotePa
 	return fileRemotePath, nil
 }
 
-func (sm *ServiceManager) DeleteRemotePathofaFile(name string, ) error {
+func (sm *ServiceManager) DeleteRemotePathofaFile(name string) error {
 	dn := fmt.Sprintf(models.DnfileRemotePath, name)
 	return sm.DeleteByDn(dn, models.FileremotepathClassName)
 }
 
 func (sm *ServiceManager) UpdateRemotePathofaFile(name string, description string, nameAlias string, fileRemotePathAttr models.RemotePathofaFileAttributes) (*models.RemotePathofaFile, error) {
-	rn := fmt.Sprintf(models.RnfileRemotePath , name)
-	parentDn := fmt.Sprintf(models.ParentDnfileRemotePath, )
+	rn := fmt.Sprintf(models.RnfileRemotePath, name)
+	parentDn := fmt.Sprintf(models.ParentDnfileRemotePath)
 	fileRemotePath := models.NewRemotePathofaFile(rn, parentDn, description, nameAlias, fileRemotePathAttr)
-    fileRemotePath.Status = "modified"
+	fileRemotePath.Status = "modified"
 	err := sm.Save(fileRemotePath)
 	return fileRemotePath, err
 }
 
-func (sm *ServiceManager) ListRemotePathofaFile() ([]*models.RemotePathofaFile, error) {	
+func (sm *ServiceManager) ListRemotePathofaFile() ([]*models.RemotePathofaFile, error) {
 	dnUrl := fmt.Sprintf("%s/uni/fabric/fileRemotePath.json", models.BaseurlStr)
-    cont, err := sm.GetViaURL(dnUrl)
+	cont, err := sm.GetViaURL(dnUrl)
 	list := models.RemotePathofaFileListFromContainer(cont)
 	return list, err
 }
@@ -63,7 +58,6 @@ func (sm *ServiceManager) CreateRelationfileRsARemoteHostToEpg(parentDn, annotat
 		}
 	}`, "fileRsARemoteHostToEpg", dn, annotation, tDn))
 
-	
 	jsonPayload, err := container.ParseJSON(containerJSON)
 	if err != nil {
 		return err
@@ -80,17 +74,17 @@ func (sm *ServiceManager) CreateRelationfileRsARemoteHostToEpg(parentDn, annotat
 	return nil
 }
 
-func (sm *ServiceManager) DeleteRelationfileRsARemoteHostToEpg(parentDn string) error{
+func (sm *ServiceManager) DeleteRelationfileRsARemoteHostToEpg(parentDn string) error {
 	dn := fmt.Sprintf("%s/rsARemoteHostToEpg", parentDn)
-	return sm.DeleteByDn(dn , "fileRsARemoteHostToEpg")
+	return sm.DeleteByDn(dn, "fileRsARemoteHostToEpg")
 }
 
-func (sm *ServiceManager) ReadRelationfileRsARemoteHostToEpg(parentDn string) (interface{},error) {	
-	dnUrl := fmt.Sprintf("%s/%s/%s.json",models.BaseurlStr,parentDn,"fileRsARemoteHostToEpg")
+func (sm *ServiceManager) ReadRelationfileRsARemoteHostToEpg(parentDn string) (interface{}, error) {
+	dnUrl := fmt.Sprintf("%s/%s/%s.json", models.BaseurlStr, parentDn, "fileRsARemoteHostToEpg")
 	cont, err := sm.GetViaURL(dnUrl)
-	contList := models.ListFromContainer(cont,"fileRsARemoteHostToEpg")
-	
-		if len(contList) > 0 {
+	contList := models.ListFromContainer(cont, "fileRsARemoteHostToEpg")
+
+	if len(contList) > 0 {
 		dat := models.G(contList[0], "tDn")
 		return dat, err
 	} else {
@@ -110,7 +104,6 @@ func (sm *ServiceManager) CreateRelationfileRsARemoteHostToEpp(parentDn, annotat
 		}
 	}`, "fileRsARemoteHostToEpp", dn, annotation, tDn))
 
-	
 	jsonPayload, err := container.ParseJSON(containerJSON)
 	if err != nil {
 		return err
@@ -127,21 +120,20 @@ func (sm *ServiceManager) CreateRelationfileRsARemoteHostToEpp(parentDn, annotat
 	return nil
 }
 
-func (sm *ServiceManager) DeleteRelationfileRsARemoteHostToEpp(parentDn string) error{
+func (sm *ServiceManager) DeleteRelationfileRsARemoteHostToEpp(parentDn string) error {
 	dn := fmt.Sprintf("%s/rsARemoteHostToEpp", parentDn)
-	return sm.DeleteByDn(dn , "fileRsARemoteHostToEpp")
+	return sm.DeleteByDn(dn, "fileRsARemoteHostToEpp")
 }
 
-func (sm *ServiceManager) ReadRelationfileRsARemoteHostToEpp(parentDn string) (interface{},error) {	
-	dnUrl := fmt.Sprintf("%s/%s/%s.json",models.BaseurlStr,parentDn,"fileRsARemoteHostToEpp")
+func (sm *ServiceManager) ReadRelationfileRsARemoteHostToEpp(parentDn string) (interface{}, error) {
+	dnUrl := fmt.Sprintf("%s/%s/%s.json", models.BaseurlStr, parentDn, "fileRsARemoteHostToEpp")
 	cont, err := sm.GetViaURL(dnUrl)
-	contList := models.ListFromContainer(cont,"fileRsARemoteHostToEpp")
-	
-		if len(contList) > 0 {
+	contList := models.ListFromContainer(cont, "fileRsARemoteHostToEpp")
+
+	if len(contList) > 0 {
 		dat := models.G(contList[0], "tDn")
 		return dat, err
 	} else {
 		return nil, err
 	}
 }
-
