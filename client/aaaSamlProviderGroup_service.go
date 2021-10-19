a6098290a4ef56b46269ab028b6e75f860bc31cb
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2021-10-19 18:54:38 +0530

    Added client and model

--- client/aaaSamlProviderGroup_service.go
+++ client/aaaSamlProviderGroup_service.go
@@ -1,25 +1,20 @@
 package client
 
-
-
 import (
-	
 	"fmt"
 
 	"github.com/ciscoecosystem/aci-go-client/models"
-	
 )
 
-
-func (sm *ServiceManager) CreateSAMLProviderGroup(name string, description string, nameAlias string, aaaSamlProviderGroupAttr models.SAMLProviderGroupAttributes) (*models.SAMLProviderGroup, error) {	
-	rn := fmt.Sprintf(models.RnaaaSamlProviderGroup , name)
-	parentDn := fmt.Sprintf(models.ParentDnaaaSamlProviderGroup, )
+func (sm *ServiceManager) CreateSAMLProviderGroup(name string, description string, nameAlias string, aaaSamlProviderGroupAttr models.SAMLProviderGroupAttributes) (*models.SAMLProviderGroup, error) {
+	rn := fmt.Sprintf(models.RnaaaSamlProviderGroup, name)
+	parentDn := fmt.Sprintf(models.ParentDnaaaSamlProviderGroup)
 	aaaSamlProviderGroup := models.NewSAMLProviderGroup(rn, parentDn, description, nameAlias, aaaSamlProviderGroupAttr)
 	err := sm.Save(aaaSamlProviderGroup)
 	return aaaSamlProviderGroup, err
 }
 
-func (sm *ServiceManager) ReadSAMLProviderGroup(name string, ) (*models.SAMLProviderGroup, error) {
+func (sm *ServiceManager) ReadSAMLProviderGroup(name string) (*models.SAMLProviderGroup, error) {
 	dn := fmt.Sprintf(models.DnaaaSamlProviderGroup, name)
 	cont, err := sm.Get(dn)
 	if err != nil {
@@ -29,24 +24,23 @@ func (sm *ServiceManager) ReadSAMLProviderGroup(name string, ) (*models.SAMLProv
 	return aaaSamlProviderGroup, nil
 }
 
-func (sm *ServiceManager) DeleteSAMLProviderGroup(name string, ) error {
+func (sm *ServiceManager) DeleteSAMLProviderGroup(name string) error {
 	dn := fmt.Sprintf(models.DnaaaSamlProviderGroup, name)
 	return sm.DeleteByDn(dn, models.AaasamlprovidergroupClassName)
 }
 
 func (sm *ServiceManager) UpdateSAMLProviderGroup(name string, description string, nameAlias string, aaaSamlProviderGroupAttr models.SAMLProviderGroupAttributes) (*models.SAMLProviderGroup, error) {
-	rn := fmt.Sprintf(models.RnaaaSamlProviderGroup , name)
-	parentDn := fmt.Sprintf(models.ParentDnaaaSamlProviderGroup, )
+	rn := fmt.Sprintf(models.RnaaaSamlProviderGroup, name)
+	parentDn := fmt.Sprintf(models.ParentDnaaaSamlProviderGroup)
 	aaaSamlProviderGroup := models.NewSAMLProviderGroup(rn, parentDn, description, nameAlias, aaaSamlProviderGroupAttr)
-    aaaSamlProviderGroup.Status = "modified"
+	aaaSamlProviderGroup.Status = "modified"
 	err := sm.Save(aaaSamlProviderGroup)
 	return aaaSamlProviderGroup, err
 }
 
-func (sm *ServiceManager) ListSAMLProviderGroup() ([]*models.SAMLProviderGroup, error) {	
+func (sm *ServiceManager) ListSAMLProviderGroup() ([]*models.SAMLProviderGroup, error) {
 	dnUrl := fmt.Sprintf("%s/uni/userext/samlext/aaaSamlProviderGroup.json", models.BaseurlStr)
-    cont, err := sm.GetViaURL(dnUrl)
+	cont, err := sm.GetViaURL(dnUrl)
 	list := models.SAMLProviderGroupListFromContainer(cont)
 	return list, err
 }
-
--- models/aaa_saml_provider_group.go
+++ models/aaa_saml_provider_group.go
@@ -6,26 +6,27 @@ import (
 
 	"github.com/ciscoecosystem/aci-go-client/container"
 )
+
 const (
-	DnaaaSamlProviderGroup                = "uni/userext/samlext/samlprovidergroup-%s"
-	RnaaaSamlProviderGroup                = "samlprovidergroup-%s"
-	ParentDnaaaSamlProviderGroup          = "uni/userext/samlext"
+	DnaaaSamlProviderGroup        = "uni/userext/samlext/samlprovidergroup-%s"
+	RnaaaSamlProviderGroup        = "samlprovidergroup-%s"
+	ParentDnaaaSamlProviderGroup  = "uni/userext/samlext"
 	AaasamlprovidergroupClassName = "aaaSamlProviderGroup"
 )
 
 type SAMLProviderGroup struct {
 	BaseAttributes
 	NameAliasAttribute
-    SAMLProviderGroupAttributes 
+	SAMLProviderGroupAttributes
 }
-  
+
 type SAMLProviderGroupAttributes struct {
-    Annotation       string `json:",omitempty"`
-    Name       string `json:",omitempty"`
-    } 
+	Annotation string `json:",omitempty"`
+	Name       string `json:",omitempty"`
+}
 
 func NewSAMLProviderGroup(aaaSamlProviderGroupRn, parentDn, description, nameAlias string, aaaSamlProviderGroupAttr SAMLProviderGroupAttributes) *SAMLProviderGroup {
-	dn := fmt.Sprintf("%s/%s", parentDn, aaaSamlProviderGroupRn)  
+	dn := fmt.Sprintf("%s/%s", parentDn, aaaSamlProviderGroupRn)
 	return &SAMLProviderGroup{
 		BaseAttributes: BaseAttributes{
 			DistinguishedName: dn,
@@ -37,7 +38,7 @@ func NewSAMLProviderGroup(aaaSamlProviderGroupRn, parentDn, description, nameAli
 		NameAliasAttribute: NameAliasAttribute{
 			NameAlias: nameAlias,
 		},
-        SAMLProviderGroupAttributes: aaaSamlProviderGroupAttr, 
+		SAMLProviderGroupAttributes: aaaSamlProviderGroupAttr,
 	}
 }
 
@@ -53,9 +54,9 @@ func (aaaSamlProviderGroup *SAMLProviderGroup) ToMap() (map[string]string, error
 	for key, value := range alias {
 		A(aaaSamlProviderGroupMap, key, value)
 	}
-    A(aaaSamlProviderGroupMap, "annotation",aaaSamlProviderGroup.Annotation)
-    A(aaaSamlProviderGroupMap, "name",aaaSamlProviderGroup.Name)
-    return aaaSamlProviderGroupMap, err
+	A(aaaSamlProviderGroupMap, "annotation", aaaSamlProviderGroup.Annotation)
+	A(aaaSamlProviderGroupMap, "name", aaaSamlProviderGroup.Name)
+	return aaaSamlProviderGroupMap, err
 }
 
 func SAMLProviderGroupFromContainerList(cont *container.Container, index int) *SAMLProviderGroup {
@@ -71,10 +72,10 @@ func SAMLProviderGroupFromContainerList(cont *container.Container, index int) *S
 		NameAliasAttribute{
 			NameAlias: G(SAMLProviderGroupCont, "nameAlias"),
 		},
-        SAMLProviderGroupAttributes{
-        Annotation : G(SAMLProviderGroupCont, "annotation"),
-        Name : G(SAMLProviderGroupCont, "name"),
-        },
+		SAMLProviderGroupAttributes{
+			Annotation: G(SAMLProviderGroupCont, "annotation"),
+			Name:       G(SAMLProviderGroupCont, "name"),
+		},
 	}
 }
 
