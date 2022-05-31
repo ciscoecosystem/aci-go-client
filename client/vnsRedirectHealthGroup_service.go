441aa022bd7877d478e0a8c8770b3adf84930531
Shreyas <ssrish@cisco.com>
2022-05-31 15:35:49 -0700

    [ignore] Change name of functions to L4L7..

--- client/vnsRedirectHealthGroup_service.go
+++ client/vnsRedirectHealthGroup_service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ciscoecosystem/aci-go-client/models"
 )
 
-func (sm *ServiceManager) CreateL4_L7RedirectHealthGroup(name string, tenant string, description string, nameAlias string, vnsRedirectHealthGroupAttr models.L4_L7RedirectHealthGroupAttributes) (*models.L4_L7RedirectHealthGroup, error) {
+func (sm *ServiceManager) CreateL4L7RedirectHealthGroup(name string, tenant string, description string, nameAlias string, vnsRedirectHealthGroupAttr models.L4_L7RedirectHealthGroupAttributes) (*models.L4_L7RedirectHealthGroup, error) {
 	rn := fmt.Sprintf(models.RnvnsRedirectHealthGroup, name)
 	parentDn := fmt.Sprintf(models.ParentDnvnsRedirectHealthGroup, tenant)
 	vnsRedirectHealthGroup := models.NewL4_L7RedirectHealthGroup(rn, parentDn, description, nameAlias, vnsRedirectHealthGroupAttr)
@@ -14,7 +14,7 @@ func (sm *ServiceManager) CreateL4_L7RedirectHealthGroup(name string, tenant str
 	return vnsRedirectHealthGroup, err
 }
 
-func (sm *ServiceManager) ReadL4_L7RedirectHealthGroup(name string, tenant string) (*models.L4_L7RedirectHealthGroup, error) {
+func (sm *ServiceManager) ReadL4L7RedirectHealthGroup(name string, tenant string) (*models.L4_L7RedirectHealthGroup, error) {
 	dn := fmt.Sprintf(models.DnvnsRedirectHealthGroup, tenant, name)
 
 	cont, err := sm.Get(dn)
@@ -26,12 +26,12 @@ func (sm *ServiceManager) ReadL4_L7RedirectHealthGroup(name string, tenant strin
 	return vnsRedirectHealthGroup, nil
 }
 
-func (sm *ServiceManager) DeleteL4_L7RedirectHealthGroup(name string, tenant string) error {
+func (sm *ServiceManager) DeleteL4L7RedirectHealthGroup(name string, tenant string) error {
 	dn := fmt.Sprintf(models.DnvnsRedirectHealthGroup, tenant, name)
 	return sm.DeleteByDn(dn, models.VnsredirecthealthgroupClassName)
 }
 
-func (sm *ServiceManager) UpdateL4_L7RedirectHealthGroup(name string, tenant string, description string, nameAlias string, vnsRedirectHealthGroupAttr models.L4_L7RedirectHealthGroupAttributes) (*models.L4_L7RedirectHealthGroup, error) {
+func (sm *ServiceManager) UpdateL4L7RedirectHealthGroup(name string, tenant string, description string, nameAlias string, vnsRedirectHealthGroupAttr models.L4_L7RedirectHealthGroupAttributes) (*models.L4_L7RedirectHealthGroup, error) {
 	rn := fmt.Sprintf(models.RnvnsRedirectHealthGroup, name)
 	parentDn := fmt.Sprintf(models.ParentDnvnsRedirectHealthGroup, tenant)
 	vnsRedirectHealthGroup := models.NewL4_L7RedirectHealthGroup(rn, parentDn, description, nameAlias, vnsRedirectHealthGroupAttr)
@@ -40,7 +40,7 @@ func (sm *ServiceManager) UpdateL4_L7RedirectHealthGroup(name string, tenant str
 	return vnsRedirectHealthGroup, err
 }
 
-func (sm *ServiceManager) ListL4_L7RedirectHealthGroup(tenant string) ([]*models.L4_L7RedirectHealthGroup, error) {
+func (sm *ServiceManager) ListL4L7RedirectHealthGroup(tenant string) ([]*models.L4_L7RedirectHealthGroup, error) {
 	dnUrl := fmt.Sprintf("%s/uni/tn-%s/svcCont/vnsRedirectHealthGroup.json", models.BaseurlStr, tenant)
 	cont, err := sm.GetViaURL(dnUrl)
 	list := models.L4_L7RedirectHealthGroupListFromContainer(cont)
