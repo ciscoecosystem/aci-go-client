20fcd0879e60facbb12fb17cd088d453e6ee629d
samitab <samitab@cisco.com>
2025-09-01 10:20:53 +1000

    [bugfix] Fix issue with extra slashes in path or base URL.

--- client/client_test.go
+++ client/client_test.go
@@ -1,25 +1,69 @@
 package client
 
 import (
+	"net/url"
 	"testing"
-
-	"github.com/ciscoecosystem/aci-go-client/v2/client"
 )
 
-func TestClientAuthenticate(t *testing.T) {
+var TestBaseUrls = [...]string{
+	"https://apic.host.cisco",
+	"https://apic.host.cisco/",
+	"https://apic.host.cisco//",
+	"https://apic.host.cisco///",
+	"https://apic.host.cisco/test",
+	"https://apic.host.cisco/test/",
+	"https://apic.host.cisco/test//",
+}
 
-	client := GetTestClient()
-	err := client.Authenticate()
+func AssertFullUrl(t *testing.T, baseUrl string, path string, expected string, preserveBaseUrl bool) {
+	url, err := url.Parse(baseUrl)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
+	}
+	aciClient := &Client{
+		BaseURL:  url,
+		preserveBaseUrlRef: preserveBaseUrl,
 	}
 
-	if client.AuthToken.Token == "" {
-		t.Error("Token is empty")
+	actual, _, err := aciClient.MakeFullUrl(path)
+	if actual != expected || err != nil {
+		t.Errorf(`MakeFullUrl("%s") = %q, %v, expected %#q`, path, actual, err, expected)
 	}
 }
 
-func GetTestClient() *client.Client {
-	return client.GetClient("https://192.168.10.102", "admin", client.Password("cisco123"), client.Insecure(true))
+func TestMakeFullUrl(t *testing.T) {
+	expected := "https://apic.host.cisco/api/mo/uni/tn-test_tenant"
+	paths := [...]string {
+		"api/mo/uni/tn-test_tenant",
+		"/api/mo/uni/tn-test_tenant",
+		"///api/mo/uni/tn-test_tenant",
+	}
+	for _, baseUrl := range TestBaseUrls {
+		for _, path := range paths {
+			AssertFullUrl(t, baseUrl, path, expected, false)
+		}
+	}
+}
 
+func TestMakeFullUrl_PreserveBaseUrl(t *testing.T) {
+	expected_path := "/api/mo/uni/tn-test_tenant"
+	paths := [...]string {
+		"api/mo/uni/tn-test_tenant",
+		"/api/mo/uni/tn-test_tenant",
+		"///api/mo/uni/tn-test_tenant",
+	}
+	for _, baseUrl := range TestBaseUrls {
+		for _, path := range paths {
+			expected := baseUrl + expected_path
+			AssertFullUrl(t, baseUrl, path, expected, true)
+		}
+	}
+}
+
+func TestMakeFullUrl_QueryParams(t *testing.T) {
+	expected := "https://apic.host.cisco/api/mo/uni/tn-test_tenant?rsp-subtree=full&rsp-prop-include=config-only"
+	path := "api/mo/uni/tn-test_tenant?rsp-subtree=full&rsp-prop-include=config-only"
+	for _, baseUrl := range TestBaseUrls {
+		AssertFullUrl(t, baseUrl, path, expected, false)
+	}
 }
