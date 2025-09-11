abc3c6eb38a5f08f36d3ea573110ae8546194075
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2025-09-11 20:43:48 +0530

    [minor_change] Added name attribute to the l3extSubnet model

--- models/l3ext_subnet.go
+++ models/l3ext_subnet.go
@@ -15,7 +15,8 @@ type L3ExtSubnet struct {
 }
 
 type L3ExtSubnetAttributes struct {
-	Ip string `json:",omitempty"`
+	Ip   string `json:",omitempty"`
+	Name string `json:",omitempty"`
 
 	Aggregate string `json:",omitempty"`
 
@@ -26,7 +27,7 @@ type L3ExtSubnetAttributes struct {
 	Scope string `json:",omitempty"`
 }
 
-func NewL3ExtSubnet(l3extSubnetRn, parentDn, description string, l3extSubnetattr L3ExtSubnetAttributes) *L3ExtSubnet {
+func NewL3ExtSubnet(l3extSubnetRn, parentDn, description, name string, l3extSubnetattr L3ExtSubnetAttributes) *L3ExtSubnet {
 	dn := fmt.Sprintf("%s/%s", parentDn, l3extSubnetRn)
 	return &L3ExtSubnet{
 		BaseAttributes: BaseAttributes{
@@ -48,6 +49,7 @@ func (l3extSubnet *L3ExtSubnet) ToMap() (map[string]string, error) {
 	}
 
 	A(l3extSubnetMap, "ip", l3extSubnet.Ip)
+	A(l3extSubnetMap, "name", l3extSubnet.Name)
 
 	A(l3extSubnetMap, "aggregate", l3extSubnet.Aggregate)
 
@@ -74,7 +76,8 @@ func L3ExtSubnetFromContainerList(cont *container.Container, index int) *L3ExtSu
 
 		L3ExtSubnetAttributes{
 
-			Ip: G(SubnetCont, "ip"),
+			Ip:   G(SubnetCont, "ip"),
+			Name: G(SubnetCont, "name"),
 
 			Aggregate: G(SubnetCont, "aggregate"),
 
